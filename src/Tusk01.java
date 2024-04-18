a1c8dd008839f8efa763e0e75629fdbef2088d81
Ratatoskr64 <Ratatoskr666@gmail.com>
2024-04-18 19:28:29 +0400

    Add Tusk01.java

---
+++ src/Tusk01.java
@@ -0,0 +1,11 @@
+public class Tusk01 {
+    public static void main(String[] args) {
+        System.out.println("Задание:#1");
+        int x = 21;
+        int y = 4;
+        int q = x / y;
+        int w = x % y;
+        System.out.println(x + " / " + y + " = ");
+        System.out.println("квадратный корень");
+    }
+}

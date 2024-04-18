424c1d4b7170644d93adc36343b12040ec13cb51
Ratatoskr64 <Ratatoskr666@gmail.com>
2024-04-18 20:03:58 +0400

    Add Tusk03.java

---
+++ src/Tusk03.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Tusk03 {
+    public static void main(String[] args) {
+        System.out.println("Задание:№3");
+        Scanner sc = new Scanner(System.in);
+        float n = Math.round(sc.nextFloat());
+    }
+}
